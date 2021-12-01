46f33c53b522904b232cd26dac657866e2d37b3f
wangjk1997 <wangjk@gatech.edu>
2021-12-01 13:27:53 -0500

    Test

---
+++ NatNetblimp.py
@@ -0,0 +1,35 @@
+import sys
+import time
+from NatNetClient import NatNetClient
+import DataDescriptions
+import MoCapData
+
+def my_parse_args(arg_list, args_dict):
+    # set up base values
+    arg_list_len=len(arg_list)
+    if arg_list_len>1:
+        args_dict["serverAddress"] = arg_list[1]
+        if arg_list_len>2:
+            args_dict["clientAddress"] = arg_list[2]
+        if arg_list_len>3:
+            if len(arg_list[3]):
+                args_dict["use_multicast"] = True
+                if arg_list[3][0].upper() == "U":
+                    args_dict["use_multicast"] = False
+
+    return args_dict
+
+class Natnet_blimp:
+    def __init__(self):
+        self.optionsDict = {}
+        self.optionsDict["clientAddress"] = "127.0.0.1"
+        self.optionsDict["serverAddress"] = "127.0.0.1"
+        self.optionsDict["use_multicast"] = True
+        # This will create a new NatNet client
+        self.optionsDict = my_parse_args(sys.argv, optionsDict)
+
+        self.streaming_client = NatNetClient()
+        self.streaming_client.set_client_address(optionsDict["clientAddress"])
+        self.streaming_client.set_server_address(optionsDict["serverAddress"])
+        self.streaming_client.set_use_multicast(optionsDict["use_multicast"])
+    
